cc7d1933ba9bf4b541e3f740e4a4540e03f2fd2d
Mradull <mradul.kapoor2004@gmail.com>
2025-05-11 23:46:57 +0530

    login fixed

--- src/auth/googleStrategy.js
+++ src/auth/googleStrategy.js
@@ -4,12 +4,15 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const User = require("../models/User");
 
+console.log("Using Google Callback URL:", process.env.GOOGLE_CALLBACK_URL);
+
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      callbackURL: "https://xeno-mini-crm-backend-ml6e.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
     try {
