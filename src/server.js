27537ecce9e58a43ba24b51040cb41f3adb98da1
Mradull <mradul.kapoor2004@gmail.com>
2025-05-12 00:13:45 +0530

    fixed frontend issues

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use(
   cors({
-    origin: "https://xeno-mini-crm-frontend.vercel.app/" || "http://localhost:5173",
+    origin: "https://xeno-mini-crm-frontend.vercel.app" || "http://localhost:5173",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
